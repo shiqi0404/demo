746bb32c1246d53bc79bc8d8420a115e458dc811
shiqi <zoushiqi0404@gmail.com>
2022-09-27 23:17:40 +0800

    torch fundamental

--- 2.torch_demo.py
+++ 2.torch_demo.py
@@ -3,7 +3,7 @@
 -------------------------------------------------
 Project Name:  demo
 File Name   :  2.torch_demo.py
-Description :  首先，我们介绍维数组，也称为张量（tensor）。 使用过Python中NumPy计算包的读者会对本部分很熟悉。
+Description :  首先，我们介绍n维数组，也称为张量（tensor）。 使用过Python中NumPy计算包的读者会对本部分很熟悉。
                无论使用哪个深度学习框架，它的张量类（在MXNet中为ndarray， 在PyTorch和TensorFlow中为Tensor）
                都与Numpy的ndarray类似。 但深度学习框架又比Numpy的ndarray多一些重要功能：
                首先，GPU很好地支持加速计算，而NumPy仅支持CPU计算；
@@ -57,7 +57,9 @@ Y = torch.tensor([[2.0, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
 print("cat_dim=0:", torch.cat((X, Y), dim=0))
 print("cat_dim=1:", torch.cat((X, Y), dim=1))
 
-print("logic:", X == Y)
+print("logic1:", X == Y)
+print("logic2:", X < Y)
+print("logic3:", X > Y)
 
 # 2.3 广播机制
 a = torch.arange(3).reshape((3, 1))
@@ -85,5 +87,34 @@ print('id(Z):', id(Z))
 Z[:] = X + Y
 print('id(Z):', id(Z))
 
+before = id(X)
+X += Y
+print(id(X) == before)
 
+# 2.6 转换为其他python对象
+A = X.numpy()
+B = torch.tensor(A)
+print(type(A), type(B))
 
+a = torch.tensor([3.5])
+print('a:', a)
+print('float(a):', float(a))
+print('a.item():', a.item())
+print('int(a):', int(a))
+
+# 2.7 张量算法的基本性质
+A = torch.arange(20, dtype=torch.float32).reshape(5, 4)
+B = A.clone() # 分配新的内存，将A的副本分配给B
+print('A:', A)
+print('A+B:', A + B)
+print('A*B:', A * B)
+
+a = 2
+X = torch.arange(24).reshape(2, 3, 4)
+print('a+X:', a + X)
+print('a*X_shape:', (a * X).shape)
+print('A:', A)
+print('A_sum_axis0:', A.sum(axis=0), 'shape:', A.sum(axis=0).shape)
+print('A_sum_axis1:', A.sum(axis=1), 'shape:', A.sum(axis=1).shape)
+print('sum_axis_all:', A.sum(axis=[0, 1]))
+print('A_cumsum:', A.cumsum(axis=0))
---
+++ 3.torch_grad.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name:  demo
+File Name   :  3.torch_grad.py
+Description :  
+Author      :  Steven.zou
+E-mail      :  zoushiqi0404@gmail.com
+Date        :  2022/9/25 23:00
+Software    :  PyCharm
+-------------------------------------------------
+Change Activity:
+
+-------------------------------------------------
+"""
+import numpy as np
+from matplotlib_inline import backend_inline
+import torch
+
+
+
